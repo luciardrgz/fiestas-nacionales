2573bc849570029c52494507bbcd9136f4e9535a
luciardrgz <lucia.rodriguez.rosales@gmail.com>
2024-12-10 19:51:52 -0300

    Add content for AdSense

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,13 +24,27 @@ export default function RootLayout({ children }) {
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3134613298811944"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="text-center bg-[#6CACE4] text-white">
+          <h1 className="text-center text-3xl font-extrabold sm:py-4 max-sm:py-2 max-sm:tracking-tighter bg-[#6CACE4] text-white">
+            Festivales Argentinos
+          </h1>
+          <p className="text-center text-sm bg-white text-red-500 leading-tight">
+            <b>IMPORTANTE:</b> 'Cómo llegar' da una ubicación <u>aproximada</u>,
+            no es exacta.
+          </p>
+        </header>
+
         {children}
+
+        <footer className="text-center p-2 bg-gray-200 text-sm">
+          © 2024 Festivales Argentinos.
+        </footer>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -27,15 +27,6 @@ export default function Home() {
 
   return (
     <section id="home" className="flex flex-col justify-center items-center w-full min-h-screen">
-      <div className="w-full">
-        <h1 className="text-center text-3xl font-extrabold sm:py-4 max-sm:py-2 max-sm:tracking-tighter bg-[#6CACE4] text-white">
-          Festivales argentinos
-        </h1>
-        <p className="text-center text-sm bg-white text-red-500 leading-tight">
-          <b>IMPORTANTE:</b> 'Cómo llegar' da una ubicación <u>aproximada</u>, no es exacta.
-        </p>
-      </div>
-
       <div className="flex flex-col items-center text-center w-[90%]">
         <Menu
           onMonthSelect={handleMonthSelect}
